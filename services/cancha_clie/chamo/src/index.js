6ef0d27e3fe8819955253549f12d86d4b7ae261e
galeanov <amadorgaleano@upeu.edu.pe>
2018-11-26 15:52:38 -0500

    casi

--- services/cancha_clie/chamo/src/index.js
+++ services/cancha_clie/chamo/src/index.js
@@ -1,19 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-      <section className="section">
-        <div className="container">
+    <section className="section">
+      <div className="container">
         <div className="columns">
-        <div className="column is-one-third">
+          <div className="column is-one-third">
             <br/>
-            <h1 className="title is-1 is-1">Todos las canchas</h1>
+            <h1 className="title is-1 is-1">Todas las canchas</h1>
             <hr/><br/>
           </div>
         </div>
@@ -22,9 +24,7 @@ const App = () => {
   )
 };
 
-
-
-ReactDOM.render( <App/>,
+ReactDOM.render(
+  <App />,
   document.getElementById('root')
 );
-
